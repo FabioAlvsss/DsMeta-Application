239c712b34d969a56803af3d3d6a14d34a6c82e9
Fabio Alves <fabio_alvss@outlook.com>
2022-09-21 20:34:24 -0300

    Date select

--- backend/src/main/java/com/fabioalves/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/fabioalves/dsmeta/controllers/SaleController.java
@@ -1,10 +1,11 @@
 package com.fabioalves.dsmeta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fabioalves.dsmeta.entities.Sale;
@@ -19,7 +20,13 @@ public class SaleController {
 	@Autowired
 	private SaleService service;
 	@GetMapping
-	public List<Sale> findSales(){
-		return service.findSales();
+	public Page<Sale> findSales(
+			@RequestParam(value = "minDate", defaultValue="")String mindate, 
+			@RequestParam(value ="maxDate", defaultValue="")String maxdate,
+			Pageable pageable){
+		return service.findSales(mindate,maxdate,pageable);
 	}
-}
+	}
+
+
+
